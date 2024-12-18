801df56132421aba010a6934dc262318dc8a7f6f
Will Hayden <haydenw@bc.edu>
2024-12-18 00:36:05 -0500

    added logging features

--- delivery3/delivery3/settings.py
+++ delivery3/delivery3/settings.py
@@ -199,3 +199,16 @@ CSRF_TRUSTED_ORIGINS = [
     'https://your-base-domain'
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',  # Log all details
+    },
+}
